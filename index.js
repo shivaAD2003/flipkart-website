6f7379715273688b293efec622cc18e24ec95257
shivaAD2003 <157198807+shivaAD2003@users.noreply.github.com>
2024-01-31 14:19:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var sidenav=document.querySelector(".side-navbar")
+sidenav.style.display="none"
+function showNavbar(){
+    sidenav.style.display='block'
+
+}
+function closeNavbar(){
+    sidenav.style.display='none'
+
+
+}
